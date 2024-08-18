2fa1f2a5d185b165498e520fb1f20f1c198b1597
Priyanshu-maxMan <jaskprince20@gmail.com>
2024-08-18 22:58:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-const http = require('https');
+const fetch = require('node-fetch');
 
+const url = 'https://hianime.p.rapidapi.com/anime/episode-srcs?id=steinsgate-3%3Fep%3D230&server=vidstreaming&category=dub';
 const options = {
-	method: 'GET',
-	hostname: 'hianime.p.rapidapi.com',
-	port: null,
-	path: '/anime/episode-srcs?id=steinsgate-3%3Fep%3D230&server=vidstreaming&category=dub',
-	headers: {
-		'x-rapidapi-key': '1c1438c1d9msh979fa7f217b0005p186e0fjsn5902434cfcb1',
-		'x-rapidapi-host': 'hianime.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': '1c1438c1d9msh979fa7f217b0005p186e0fjsn5902434cfcb1',
+    'x-rapidapi-host': 'hianime.p.rapidapi.com'
+  }
 };
 
-const req = http.request(options, function (res) {
-	const chunks = [];
-
-	res.on('data', function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on('end', function () {
-		const body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
-});
-
-req.end();
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
